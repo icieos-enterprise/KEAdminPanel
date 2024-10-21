8e849427920e8056b5343e9c97b9993ce5ced05b
sahan542 <sahanrashmika542@gmail.com>
2024-10-21 10:13:36 +0530

    project structure

---
+++ admin/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className='bg-green-600 text-white text-center '>Admin Panel</h1>
+    </>
+  );
+}
+
+export default App;
